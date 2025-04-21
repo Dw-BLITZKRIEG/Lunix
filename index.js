9e8c4926f35968c273138e0a32c0bac2f4d0a3e6
Glitch (arras-mayhem) <none>
2025-04-21 20:25:38 +0000

    🐾💧 Checkpoint ./index.js:28824481/711

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require("express"); 
+const fs = require('fs');
 var app = express(); 
 var path = require("path"); 
 app.get('/',function(req,res){ 
@@ -8,3 +9,28 @@ app.get('/',function(req,res){
 app.use(express.static(__dirname + '/'));
 app.listen(3000); 
 console.log("Server running at Port 3000"); 
+
+
+
+// Trust proxy to get real IP from Glitch (Glitch uses proxies)
+app.set('trust proxy', true);
+
+// Log IPs on every request
+app.use((req, res, next) => {
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  console.log(`New visitor: ${ip}`);
+
+  // Save to a file (in-memory only on Glitch)
+  fs.appendFileSync('ip-log.txt', `${new Date().toISOString()} - ${ip}\n`);
+
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello from Glitch! Your IP has been logged.');
+});
+
+// Keep Glitch app alive
+const listener = app.listen(3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
