a2a1ad8423a49485ae73f5fa239d1561ed013027
Dw-BLITZKRIEG <90556731+Dw-BLITZKRIEG@users.noreply.github.com>
2025-10-28 13:20:24 +0100

    b

--- bundle.js
+++ bundle.js
@@ -4150,7 +4150,7 @@ let color = ["#ff0000"]
                // code
                    N.draw(
                    
-                      "Kills: " + (a[0]) ?? 0,
+                      "Kills: " + c(a[0]) ?? 0,
                     b.screenWidth - 150, // example: right side
                       280,                  // example: from top
                       30,                  // font size
