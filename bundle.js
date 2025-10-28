6e77b9ea93cd0e74b00641e0f2b509bef1dce56d
Dw-BLITZKRIEG <90556731+Dw-BLITZKRIEG@users.noreply.github.com>
2025-10-28 13:36:29 +0100

    d

--- bundle.js
+++ bundle.js
@@ -3927,7 +3927,7 @@ let color = ["#ff0000"]
 
 
       N.draw(
-                  "kills:" + c.a[0],
+                  "kills:" + c.a,
                   300,
                   67,
                   32,
