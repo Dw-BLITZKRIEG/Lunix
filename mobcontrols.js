446c843392bbdb235d197a82e219ae9fa529481c
Dw-BLITZKRIEG <90556731+Dw-BLITZKRIEG@users.noreply.github.com>
2025-12-10 20:20:40 +0100

    a

--- mobcontrols.js
+++ mobcontrols.js
@@ -6,6 +6,44 @@ toggle.addEventListener("click", () => {
     menu.classList.toggle("hidden");
 });
 
+
+
+
+const buttons = ["auto-btn", "spin-btn"]; // add other buttons here
+buttons.forEach(id => {
+    const btn = document.getElementById(id);
+    btn.addEventListener("mousedown", e => e.preventDefault());
+});
+
+
+
+
+document.getElementById("auto-btn").onclick = () => {
+    window.mobileAction = "AUTO_FIRE";
+    document.dispatchEvent(new KeyboardEvent("keydown", { keyCode: 0 }));
+    document.getElementById("gameCanvas").focus(); // focus back
+};
+
+document.getElementById("spin-btn").onclick = () => {
+    window.mobileAction = "AUTO_SPIN";
+    document.dispatchEvent(new KeyboardEvent("keydown", { keyCode: 0 }));
+    document.getElementById("gameCanvas").focus();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 document.getElementById("auto-btn").onclick = () => {
     window.mobileAction = "AUTO_FIRE";
     document.dispatchEvent(new KeyboardEvent("keydown", { keyCode: 0 })); // just to trigger the handler
@@ -15,3 +53,4 @@ document.getElementById("spin-btn").onclick = () => {
     window.mobileAction = "AUTO_SPIN";
     document.dispatchEvent(new KeyboardEvent("keydown", { keyCode: 0 }));
 };
+*/
